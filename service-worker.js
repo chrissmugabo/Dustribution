67bb48577af3ede62da7c26af511d33bd53ea9e7
Valens <kwizeravalens62@hotmail.com>
2023-06-17 21:02:19 +0200

    Add production bundles

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8a25b2d2d06ea92ac96004f38407b15.js"
+  "/precache-manifest.36d25a78306b239460492de1e4a0da9c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tame-apps"});
